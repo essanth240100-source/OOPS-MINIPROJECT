e3876afe00df1f25e0bb90f0c0c0131e6f5480d1
essanth240100-source <essanth.240100@cse.ritchennai.edu.in>
2025-11-06 11:04:00 +0530

    Delete backend directory

--- backend/src/main/java/com/travelorganizer/repository/TripRepository.java
+++
@@ -1,6 +0,0 @@
-package com.travelorganizer.repository;
-
-import com.travelorganizer.model.Trip;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TripRepository extends JpaRepository<Trip, Long> { }
